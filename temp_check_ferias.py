784cccbd45629a8111295caa8249db17ff0b18ac
dfosmed <smed.dfo@valadares.mg.gov.br>
2026-06-25 09:49:08 -0300

    Remover arquivos temporarios de script do repositorio

--- temp_check_ferias.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-df = pd.read_excel('outputs/SMED - C. Custo e Regime (5)_classificado.xlsx', 'Resumo Geral')
-c_custo = "038 - OUTROS SMED"
-regimes = ["009 - CONTRATO", "020 - PROFISSIONAL EDUCACAO (CONT.)"]
-df_block = df[(df['C.Custo'] == c_custo) & (df['Regime'].isin(regimes))]
-print(df_block[df_block['Natureza'].str.contains('FERIAS', case=False, na=False)])
-print(df_block[df_block['Natureza'].str.contains('ABONO CONSTITUCIONAL', case=False, na=False)])
--- test_script.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from src.gerador_contabilizacao import parse_and_fill_contabilizacao
-
-df = pd.read_excel('outputs/SMED - C. Custo e Regime (5)_classificado.xlsx', 'Resumo Geral')
-res = parse_and_fill_contabilizacao(df, 'modelo_contabilização.xlsx')
-with open('outputs/Contabilização Folha.xlsx', 'wb') as f:
-    f.write(res)
-print('Saved output')
